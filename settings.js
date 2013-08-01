989b6563a1cb069d921ee9a8097b63de6568530f
Eric Fontenault <eric.m.font@gmail.com>
2013-08-01 13:50:13 -0500

    Removed whitespace

--- settings.js
+++ settings.js
@@ -5,14 +5,12 @@ window.sifterPlugin = {
             "resolve" : "markResolved",
             "resolves": "markResolved",
             "resolved": "markResolved",
-
             "open"    : "markOpened"  ,
             "opens"   : "markOpened"  ,
             "opened"  : "markOpened"  ,
             "reopen"  : "markOpened"  ,
             "reopens" : "markOpened"  ,
             "reopened": "markOpened"  ,
-
             "close"   : "markClosed"  ,
             "closes"  : "markClosed"  ,
             "closed"  : "markClosed"
