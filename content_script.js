8aabc38c54b0ecb026c5eeded7b988cb9dce40af
Samuel Dillow <tarwich@yahoo.com>
2013-08-01 13:22:42 -0500

    Reworked this plugin to be a content script

---
+++ content_script.js
@@ -0,0 +1,8 @@
+
+$(function() {
+});
+
+window.sifterPlugin.markResolved = function() {
+
+};
+
---
+++ settings.js
@@ -0,0 +1,9 @@
+
+window.sifterPlugin = {
+	settings: {
+		hashActions: {
+			"resolve": "markResolved",
+		}
+	}
+};
+
